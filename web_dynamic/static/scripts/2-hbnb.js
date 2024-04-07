d0acf8f7093f402400f28250a354500c607da87b
Lotfy98 <mohamed.lotfy78@gmail.com>
2024-04-08 01:57:04 +0200

    ةثىيهىل

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 // checkbox and display amenities
 $(function () {
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
     if (data['status'] === 'OK') {
       $('DIV#api_status').addClass('available');
     } else {
